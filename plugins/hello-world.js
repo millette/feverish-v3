dac110f588148e6f4fb3567a5d54b74438f568a5
Robin Millette <robin@millette.info>
2016-10-07 17:15:21 -0400

    Initial commit.

---
+++ plugins/hello-world.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const after = (options, server, next) => {
+  console.log('after...')
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      handler: { view: { template: 'bienvenue' } },
+      description: 'Home sweet home (desc)',
+      notes: 'Home sweet home, a note',
+      tags: ['fi', 'fe', 'fo']
+    }
+  })
+
+  next()
+}
+
+exports.register = (server, options, next) => {
+  console.log('register...')
+  server.dependency(['vision', 'visionary'], after.bind(null, options))
+  next()
+}
+
+exports.register.attributes = {
+  name: 'hello-world',
+  version: '0.0.0'
+}
